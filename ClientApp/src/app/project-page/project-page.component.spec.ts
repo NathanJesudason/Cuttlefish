fd2634484b6b1a51010f6ef87913cea41faff76f
Sebastian Hardin <sebastian.hardin@gmail.com>
2022-12-05 14:44:11 -0800

    Add a basic unit test for the projectpagecomponent

---
+++ ClientApp/src/app/project-page/project-page.component.spec.ts
@@ -0,0 +1,20 @@
+import {
+  MockBuilder,
+  MockRender,
+  ngMocks
+} from 'ng-mocks';
+import { AccordionModule } from 'primeng/accordion';
+
+// import { TaskData } from '../../types/task';
+import { ProjectPageComponent } from './project-page.component';
+
+describe('ProjectPageComponent', () => {
+  beforeEach(() => MockBuilder(ProjectPageComponent, AccordionModule));
+
+  it('should create', () => {
+    MockRender(ProjectPageComponent);
+    expect(ngMocks.findAll(ProjectPageComponent)[0]).toBeTruthy();
+  });
+
+  // if we ever put anything in the projectpage besides a single accordion, add more unit tests for that
+});
