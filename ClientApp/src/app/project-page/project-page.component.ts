0734c2235b9f6c00f16ac43ee008070704b925a7
Sebastian Hardin <60301051+hardins01@users.noreply.github.com>
2022-12-15 16:01:59 -0700

    Some basic frontend cleanup (#52)
    Just a little bit of frontend cleanup:
    
    - Update bootstrap (did some looking around and turns out we still make use of it)
    - ModuleMapLoaderModule has been deprecated, so remove it
    - Remove commented HTML that I should have removed in my last pr
    - Remove deprecated fields from angular.json
    - Remove an unused import that I left behind at some point

--- ClientApp/src/app/project-page/project-page.component.ts
+++ ClientApp/src/app/project-page/project-page.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { SprintDropdownComponent } from '../sprint-dropdown/sprint-dropdown.component';
-
 @Component({
   selector: 'project-page',
   templateUrl: './project-page.component.html',
