853f0f4a9dc383714a4577b0a3aa631317675577
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-12-29 19:51:46 -0800

    Continue creating queries

--- SQLQueries/queries.sql
+++ SQLQueries/queries.sql
@@ -1,27 +1,30 @@
 -- Queries containing ":" denote that the variable will be supplied from the backend
 
 -- Get all data from a table
-SELECT sprintID, startDate, endDate, Goal, storyPointsCompleted, storyPointsAttempted, projectID, IsBacklog from Sprint
-SELECT projectID, name, color, description, dueDate, funds from Project
-SELECT username, email, role, avatar from team_member
-
---Get all task info and comments for a task
-SELECT * from task where taskID = :curTaskID
-SELECT * from comment where task = :curTaskID ORDER BY date desc
+SELECT idsprint, startDate, endDate, Goal, pointsCompleted, pointsAttempted, idproject, IsBacklog, Name from sprint
+SELECT ProjectID, Name, Color, Description, DueDate, Funds from project
+SELECT team_memberID, Username, email, Role, Avatar_file from team_member
+    -- Get all task info and comments for a task
+SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where taskID = :curTaskID
+SELECT idcomment, Author, Date, Content from comment where task = :curTaskID ORDER BY date desc
 
 -- Insert item into table
-INSERT INTO task
-INSERT INTO comment
-INSERT INTO Project
+INSERT INTO task () values ();
+INSERT INTO comment () values ();
+INSERT INTO Project () values ();
+INSERT INTO team_member () values ();
 
 -- Find specific item from a table
     -- Get all tasks assigned to a person
-    SELECT * from task where assigneeID = :curUsername
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where assigneeID = :curUsername
     -- Get all tasks based on status/label
-    SELECT * from task where label = :curLabel
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where label = :curLabel
+    -- Get all tasks in a sprint
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where sprintID = :curSprintID
 
 -- Update item in a table
-UPDATE task SET name = :currName, ... , WHERE taskID = curTaskID
+UPDATE task SET Name = :curName, Type = :curType, idParentTask = :curParentID, dueDate = :curDueDate, assigneeTMID = :curAssignID, sprintID = :curSprintID, cost = :curCostID, projectID := curProjectID, Description := curDescription, Priority = :curPriority, storyPoints = :curPoints, Status = :curStatus, completionApproval := curApproval, EpicLabel := curEpicLabel, startDate := curStartDate, endDate := curEndDate, WHERE taskID = curTaskID
 
 -- Delete item from a table
+DELETE FROM task WHERE idtask = :curIDtask;
 
