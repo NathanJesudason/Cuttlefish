ba2c15151cfbbf6a53d866ac8689a04752f71dae
Nathan Jesudason <nathan.jesudason@gmail.com>
2022-12-31 17:42:00 -0800

    Add more queries

--- SQLQueries/queries.sql
+++ SQLQueries/queries.sql
@@ -5,26 +5,36 @@ SELECT idsprint, startDate, endDate, Goal, pointsCompleted, pointsAttempted, idp
 SELECT ProjectID, Name, Color, Description, DueDate, Funds from project
 SELECT team_memberID, Username, email, Role, Avatar_file from team_member
     -- Get all task info and comments for a task
-SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where taskID = :curTaskID
-SELECT idcomment, Author, Date, Content from comment where task = :curTaskID ORDER BY date desc
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where taskID = :curTaskID
+    SELECT idcomment, Author, Date, Content from comment where task = :curTaskID ORDER BY date desc
 
 -- Insert item into table
-INSERT INTO task () values ();
-INSERT INTO comment () values ();
-INSERT INTO Project () values ();
-INSERT INTO team_member () values ();
+    -- Insert task will also need to insert task dependency if it is a subtask or is being set to depend on another task
+    INSERT INTO task (Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name) values (:curType, :curidParentTask, :curdueDate, :curassigneeTMID, :curcreatorTMID, :cursprintID, :curcost, :curprojectID, :curDescription, :curPriority, :curstoryPoints, :curStatus, :curcompletionApproval, :curEpicLabel, :curstartDate, :curendDate, :curName);
+    INSERT INTO task_dependency (independent_task, dependent_task) values (:curIndepent_task, :curDependent_task)
+INSERT INTO comment (Author, Date, Task, Project, Content) values (:curAuthor, :curDate, :curTaskID, :curProjectID, :curContent);
+INSERT INTO Project (Name, Color, Description, DueDate, Funds) values (:curName, :curColor, :curDescription, :curDueDate, :curFunds);
+INSERT INTO team_member (Username, Password, email, Role, Avatar_file) values (:curUsername, :curPassword, :curEmail, :curRole, :curAvatar);
+
 
 -- Find specific item from a table
     -- Get all tasks assigned to a person
-    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where assigneeID = :curUsername
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where assigneeID = :curUsername;
     -- Get all tasks based on status/label
-    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where label = :curLabel
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where label = :curLabel;
     -- Get all tasks in a sprint
-    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where sprintID = :curSprintID
-
+    SELECT idtask, Type, idParentTask, dueDate, assigneeTMID, creatorTMID, sprintID, cost, projectID, Description, Priority, storyPoints, Status, completionApproval, EpicLabel, startDate, endDate, Name from task where sprintID = :curSprintID;
+    -- Get all team members on a project
+    SELECT team_memberID, Username, email, Role, Avatar_file from team_member WHERE team_memberID IN (SELECT team_member FROM team_member_to_project WHERE team_member_to_project.project = :curProjectID);
 -- Update item in a table
 UPDATE task SET Name = :curName, Type = :curType, idParentTask = :curParentID, dueDate = :curDueDate, assigneeTMID = :curAssignID, sprintID = :curSprintID, cost = :curCostID, projectID := curProjectID, Description := curDescription, Priority = :curPriority, storyPoints = :curPoints, Status = :curStatus, completionApproval := curApproval, EpicLabel := curEpicLabel, startDate := curStartDate, endDate := curEndDate, WHERE taskID = curTaskID
+UPDATE team_member SET Username = :curUsername, Password = :curPassword, email = :curEmail, Role = :curRole, Avatar_file = :curAvatar WHERE team_memberID = :curTMID;
+UPDATE comment SET Content = :curContent WHERE idcomment = :curCommentID;
+UPDATE task_dependency SET independent_task = :curIndepent_task, dependent_task = :curDependent_task WHERE independent_task = :old_independent_task AND dependent_task = :old_dependent_task
 
 -- Delete item from a table
 DELETE FROM task WHERE idtask = :curIDtask;
-
+DELETE FROM team_member where team_memberID where :curTMID;
+DELETE FROM comment where idcomment = :curCommentID;
+DELETE FROM project WHERE idproject = :curProjectID;
+DELETE FROM task_dependency WHERE independent_task = :curIndepent_task AND dependent_task = :curDependent_task;
