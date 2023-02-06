0d9090c90f30b7f3b32a749a03a680e78e827488
Sebastian Hardin <sebastian.hardin@gmail.com>
2023-02-05 16:18:56 -0800

    Populate create sprint modal with forms

--- ClientApp/src/app/create-sprint-modal/create-sprint-modal.component.ts
+++ ClientApp/src/app/create-sprint-modal/create-sprint-modal.component.ts
@@ -8,11 +8,16 @@ import { MessageService } from 'primeng/api';
 @Component({
   selector: 'create-sprint-modal',
   templateUrl: './create-sprint-modal.component.html',
-  styleUrls: ['./create-sprint-modal.component.css'],
+  styleUrls: ['./create-sprint-modal.component.scss'],
   providers: [MessageService],
 })
 export class CreateSprintModalComponent implements OnInit {
   createSprintModalShown: boolean = false;
+
+  inputName!: string;
+  inputStartDate!: Date | null;
+  inputEndDate!: Date | null;
+  inputIsBacklog: boolean = false;
 
   constructor(
     private messageService: MessageService,
@@ -27,10 +32,15 @@ export class CreateSprintModalComponent implements OnInit {
 
   hideCreateSprintModal() {
     this.createSprintModalShown = false;
+    this.inputName = "";
+    this.inputStartDate = null;
+    this.inputEndDate = null;
+    this.inputIsBacklog = false;
   }
 
   acceptModalInput() {
-    this.messageService.add({severity: 'success', summary: 'Input accepted!'});
+    this.messageService.add({severity: 'success', summary: `Input accepted! name: ${this.inputName}`});
+    // call to serverapi with the collected input* values
     this.hideCreateSprintModal();
   }
 
