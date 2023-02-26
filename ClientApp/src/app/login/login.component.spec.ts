5c764211d8e9ef4aabd663be4539b80775d748b1
Sebastian Hardin <sebastian.hardin@gmail.com>
2023-02-26 15:43:00 -0800

    Fix unit tests of login and signup pages

--- ClientApp/src/app/login/login.component.spec.ts
+++ ClientApp/src/app/login/login.component.spec.ts
@@ -1,27 +1,43 @@
 import { HttpResponse } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
-import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import {
+  MockBuilder,
+  MockRender,
+  ngMocks
+} from 'ng-mocks';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+
 import { of } from 'rxjs';
+
 import { AuthService } from '../server-api/auth.service';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
   let body = {username: "username", password: "Username123."}
 
   beforeEach(async () => {
-    return MockBuilder(LoginComponent).mock(
-      AuthService,{
+    return MockBuilder(LoginComponent, [
+      RouterModule,
+      ReactiveFormsModule,
+      PasswordModule,
+      ButtonModule,
+      CardModule,
+    ]).mock(AuthService, {
         login: (_loginform: AbstractControl) => {
           return of(new HttpResponse({status: 200, body: body} ))
         }
-      } 
-    ).keep(
-      FormBuilder
-    )
+      }
+    ).keep(FormBuilder, { export: true });
   });
 
   it('should create', () => {
--- ClientApp/src/app/signup/signup.component.spec.ts
+++ ClientApp/src/app/signup/signup.component.spec.ts
@@ -1,28 +1,22 @@
 import { HttpResponse } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AbstractControl, FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { of } from 'rxjs';
-import { LoginComponent } from '../login/login.component';
 import { AuthService } from '../server-api/auth.service';
 
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
   let body = {message: 'string'}
 
   beforeEach(async () => {
-    return MockBuilder(SignupComponent).mock(
+    return MockBuilder(SignupComponent, [ReactiveFormsModule]).mock(
       AuthService,{
         signUp: (_loginform: AbstractControl) => {
           return of(new HttpResponse({status: 200, body: body} ))
         }
       } 
-    ).keep(
-      FormBuilder
-    )
+    ).keep(FormBuilder, { export: true });
   });
 
   it('should create', () => {
