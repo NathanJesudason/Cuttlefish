1b5a66f660e25c5850dd04d29236ec1665510905
Stef Timmermans <63319043+stef-timmermans@users.noreply.github.com>
2023-03-01 16:39:37 -0800

    Feb calendar inplace (#102)
    * Implemented buttons for calendar on task page
    
    * Begun implementation of dropdown for progress
    
    * Fixed most of the .ts file
    
    * Fixed changes to .ts file to test on front-end
    
    * Included cscc, which seems to not need changes
    
    * Issues with *ngFor
    
    * Fixed compile errors, overlay panel still blank
    
    * pre-redo of html if change breaks
    
    * Somewhat functional
    
    * It's 3:30 and I need sleep.
    
    * Need fix for cancel message
    
    * Fixed and renamed 'ProgressPicker', npm test good

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { DialogModule } from 'primeng/dialog';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ColorPickerModule } from 'primeng/colorpicker';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { DropdownModule } from 'primeng/dropdown';
 
 import { NgxGanttModule } from '@worktile/gantt';
@@ -60,6 +61,7 @@ import { DescriptionInplaceComponent } from './description-inplace/description-i
 import { CreateSprintModalComponent } from './create-sprint-modal/create-sprint-modal.component';
 import { CreateTaskModalComponent } from './create-task-modal/create-task-modal.component';
 import { CreateProjectModalComponent } from './create-project-modal/create-project-modal.component';
+import { ProgressPickerComponent } from './progress-picker/progress-picker.component';
 import { LabelsPageComponent } from './labels-page/labels-page.component';
 
 
@@ -90,7 +92,9 @@ import { LabelsPageComponent } from './labels-page/labels-page.component';
     CreateSprintModalComponent,
     CreateTaskModalComponent,
     CreateProjectModalComponent,
+    ProgressPickerComponent,
     LabelsPageComponent,
+    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -124,7 +128,9 @@ import { LabelsPageComponent } from './labels-page/labels-page.component';
     ColorPickerModule,
     NgxGanttModule,
     EditorModule,
+    OverlayPanelModule,
     DropdownModule,
+
     RouterModule.forRoot([
       { path: '', component: HomePageComponent, pathMatch: 'full' },  // landing page for site
       { path: 'login', component: LoginComponent},
