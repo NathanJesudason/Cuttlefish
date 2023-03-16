1d0c36241c882a810c1d30723d734923c7921552
yevgeniychaplygin3 <ychaplygin@yahoo.com>
2023-03-15 18:16:48 -0700

    update team members page to show list of project id's from database

--- ClientApp/src/app/components/pages/team-members/team-members.component.ts
+++ ClientApp/src/app/components/pages/team-members/team-members.component.ts
@@ -21,8 +21,11 @@ export class TeamMembersComponent implements OnInit {
   constructor(public service: TeamMemberService, private auth: AuthService, private user: UserService, public teammemberToProjectService: TeamMemberToProjectService) { }
 
   public role!: string
-  // public teamMembersToProjectList!: TeamMemberToProject[]
-  // public teamMembersToProject!: TeamMemberToProject
+  public list!: TeamMemberToProject[]
+  public listofProjects: number[] = []
+  public listofProjects2: number[] = []
+  public listofMembers: number[] = []
+  public teamMembersToProject!: TeamMemberToProject
 
   ngOnInit(): void {
     if(this.auth.isLoggedIn())
@@ -35,17 +38,36 @@ export class TeamMembersComponent implements OnInit {
         this.role = value || rolefromToken
         
       })
-
-    this.teammemberToProjectService.refreshList()
+      this.populateTable()
+      
   }
 
-  populateForm(selectedItem: TeamMember){
-    this.service.teamMemberData = Object.assign({}, selectedItem)
-  }
-
+  
+  
   populateTable(){
-    // this.service.getTeamMemberByID(this.teammemberToProjectService.)
     //stopped at getting data for project name and team member by it and displaying it on the table. 
+    this.teammemberToProjectService.refreshList().subscribe(
+      res =>{
+          this.teammemberToProjectService.teamMemberToProjectList = res as TeamMemberToProject[]
+          this.list = this.teammemberToProjectService.teamMemberToProjectList
+          console.log("this.listtt", this.list)
+
+          // assign two arrays: one for projectIDs and one for teammemberIDs
+          this.teammemberToProjectService.teamMemberToProjectList.map(
+            list =>{
+              this.listofProjects.push(list.projectID)
+              this.listofMembers.push(list.teamMemberID)
+            }
+          )
+            // remove duplicate values
+          this.listofProjects = [...new Set(this.listofProjects)]
+          this.listofMembers = [...new Set(this.listofMembers)]
+
+          
+          console.log("this.list", this.list, this.listofProjects2, this.listofMembers)
+          
+      }
+    )
   }
    
 
@@ -53,7 +75,7 @@ export class TeamMembersComponent implements OnInit {
   onDelete(id:number){
     if(confirm('Delete this team member?')){
       this.service.deleteTeamMember(id).subscribe(
-        res => {
+       ( res) => {
           this.service.refreshList()
           console.log("Team member deleted")
         }
