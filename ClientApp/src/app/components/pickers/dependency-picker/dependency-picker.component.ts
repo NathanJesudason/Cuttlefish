17935d8c18bb447921248a7249c06833516192dc
Stef Timmermans <timmerms@oregonstate.edu>
2023-04-12 08:38:54 -0700

    Added basic add/remove funct

--- ClientApp/src/app/components/pickers/dependency-picker/dependency-picker.component.ts
+++ ClientApp/src/app/components/pickers/dependency-picker/dependency-picker.component.ts
@@ -22,30 +22,56 @@ import {
   
     @Input() data!: TaskData;
   
-    progressOptions!: string[] | undefined;
-    selectedProgress!: string;
+    dependencyOptions!: number[] | undefined;
+    selectedDependency!: number;
   
     constructor(
       private messageService: MessageService,
     ) { }
   
     ngOnInit() {
-      this.progressOptions = this.data.dependencies;
+      this.dependencyOptions = this.data.dependencies;
     }
   
-    showOption(option: string) {
-      this.selectedProgress = option;
-      this.overlayPanel.hide();
+    showOption(option: number) {
+      this.selectedDependency = option;
       this.approveChanges(option);
     }
   
+    addDependency(dependencyId: number): number {
+      if (!this.data.dependencies) {
+        this.data.dependencies = [];
+      }
+    
+      if (this.data.dependencies.includes(dependencyId)) {
+        return -1; // dependency already exists
+
+        /*
+        Eventually, we'll want to check if the given dependency
+        exists somewhere in the database. This component should
+        not have the power to create new tasks, so if it attempts
+        to add a dependency that doesn't exist within ProjectData,
+        then this should return -1 and addDependency should not
+        succeed.
+        */
+      }
+    
+      this.data.dependencies.push(dependencyId);
+      return 1; // successful
+    }
+
     approveChanges(event: any) {
-      this.messageService.add({severity: 'success', summary: `Progress was changed to ${this.selectedProgress}!`});
-      // when the time comes, add a serverApi call here to send change to backend
+      const result = this.addDependency(123); // replace with actual dependency id from HTML
+      if (result === -1) {
+        this.messageService.add({severity: 'error', summary: 'Dependency already exists!'});
+      } else {
+        this.overlayPanel.hide();
+        this.messageService.add({severity: 'success', summary: `Dependency added successfully!`});
+      }
     }
   
     cancelInput() {
       this.overlayPanel.hide();
-      this.messageService.add({severity: 'info', summary: 'Progress update was cancelled'});
+      this.messageService.add({severity: 'info', summary: 'Dependency addition was cancelled'});
     }
   }
