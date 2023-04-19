73f27af205a83fd841646501d8d869bfe59cb334
Stef Timmermans <timmerms@oregonstate.edu>
2023-04-19 09:28:10 -0700

    Added rudimentary REMOVE dependency

--- ClientApp/src/app/components/pickers/delete-dependency-picker/delete-dependency-picker.component.ts
+++ ClientApp/src/app/components/pickers/delete-dependency-picker/delete-dependency-picker.component.ts
@@ -1,63 +1,63 @@
 import {
-    Component,
-    OnInit,
-    Input,
-    ViewChild,
-  } from '@angular/core';
-  
-  import { MessageService } from 'primeng/api';
-  import { OverlayPanel } from 'primeng/overlaypanel';
-  
-  import { TaskData } from 'src/types/task';
-  
-  @Component({
-    selector: 'delete-dependency-picker',
-    templateUrl: './delete-dependency-picker.component.html',
-    styleUrls: ['./delete-dependency-picker.component.scss'],
-    providers: [MessageService],
-  })
-  export class DeleteDependencyPickerComponent implements OnInit {
-    @ViewChild('overlayPanel')
-    overlayPanel!: OverlayPanel;
-  
-    @Input() data!: TaskData;
-  
-    dependencyOptions!: number[] | undefined;
-    selectedDependency!: number;
-  
-    constructor(
-      private messageService: MessageService,
-    ) { }
-  
-    ngOnInit() {
-      this.dependencyOptions = this.data.dependencies;
-    }
-  
-    showOption(option: number) {
-      this.selectedDependency = option;
-      this.approveChanges();
-    }
-  
-    approveChanges() {
-      const result = this.removeDependency(this.selectedDependency);
-      if (result === 1) {
-        this.messageService.add({severity: 'success', summary: `Dependency ${this.selectedDependency} was successfully removed!`});
-      } else {
-        this.messageService.add({severity: 'error', summary: `Dependency ${this.selectedDependency} does not exist!`});
-      }
-    }
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+} from '@angular/core';
 
-    removeDependency(dependency: number): number {
-      const index = this.data.dependencies?.indexOf(dependency);
-      if (index !== undefined && index !== -1) {
-        this.data.dependencies?.splice(index, 1);
-        return 1;
-      }
-      return -1;
-    }
-  
-    cancelInput() {
-      this.overlayPanel.hide();
-      this.messageService.add({severity: 'info', summary: 'Progress update was cancelled'});
+import { MessageService } from 'primeng/api';
+import { OverlayPanel } from 'primeng/overlaypanel';
+
+import { TaskData } from 'src/types/task';
+
+@Component({
+  selector: 'delete-dependency-picker',
+  templateUrl: './delete-dependency-picker.component.html',
+  styleUrls: ['./delete-dependency-picker.component.scss'],
+  providers: [MessageService],
+})
+export class DeleteDependencyPickerComponent implements OnInit {
+  @ViewChild('overlayPanel')
+  overlayPanel!: OverlayPanel;
+
+  @Input() data!: TaskData;
+
+  dependencyOptions!: number[] | undefined;
+  selectedDependency!: number;
+
+  constructor(
+    private messageService: MessageService,
+  ) { }
+
+  ngOnInit() {
+    this.dependencyOptions = this.data.dependencies;
+  }
+
+  showOption(option: number) {
+    this.selectedDependency = option;
+    this.approveChanges();
+  }
+
+  approveChanges() {
+    const result = this.removeDependency(this.selectedDependency);
+    if (result === 1) {
+      this.messageService.add({severity: 'success', summary: `Dependency ${this.selectedDependency} was successfully removed!`});
+    } else {
+      this.messageService.add({severity: 'error', summary: `Dependency ${this.selectedDependency} does not exist!`});
     }
   }
+
+  removeDependency(dependency: number): number {
+    const index = this.data.dependencies?.indexOf(dependency);
+    if (index !== undefined && index !== -1) {
+      this.data.dependencies?.splice(index, 1);
+      return 1;
+    }
+    return -1;
+  }
+
+  cancelInput() {
+    this.overlayPanel.hide();
+    this.messageService.add({severity: 'info', summary: 'Dependency deletion was cancelled'});
+  }
+}
