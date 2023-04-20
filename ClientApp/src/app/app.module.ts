9ef3215b1a7d2c5ed169402d9ee878c66bb31647
Sebastian Hardin <60301051+hardins01@users.noreply.github.com>
2023-04-20 15:42:51 -0700

    New task dropdown (#147)
    Convert task dropdown to `task-overview`, with more succinct information in a tighter package. No longer a dropdown, just a div that kinda looks like the menubar. I added the drag handle for when we implement drag and drop, but I wanted to get this pr through first.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { TooltipModule } from 'primeng/tooltip';
 import { ListboxModule } from 'primeng/listbox';
 import { MultiSelectModule } from 'primeng/multiselect';
+import { DragDropModule } from 'primeng/dragdrop';
 
 import { NgxGanttModule } from '@worktile/gantt';
 
@@ -46,7 +47,7 @@ import { NavMenuComponent } from 'src/app/components/miscellaneous/nav-menu/nav-
 import { HomePageComponent } from 'src/app/components/pages/home-page/home-page.component';
 import { SprintDropdownComponent } from 'src/app/components/miscellaneous/sprint-dropdown/sprint-dropdown.component';
 import { ProjectPageComponent } from 'src/app/components/pages/project-page/project-page.component';
-import { TaskDropdownComponent } from 'src/app/components/miscellaneous/task-dropdown/task-dropdown.component';
+import { TaskOverviewComponent } from 'src/app/components/miscellaneous/task-overview/task-overview.component';
 import { TaskPageComponent } from 'src/app/components/pages/task-page/task-page.component';
 import { GanttPageComponent } from 'src/app/components/pages/gantt-page/gantt-page.component';
 import { NotFoundPageComponent } from 'src/app/components/pages/not-found-page/not-found-page.component';
@@ -81,7 +82,7 @@ import { TaskTypePickerComponent } from './components/pickers/task-type-picker/t
     HomePageComponent,
     SprintDropdownComponent,
     ProjectPageComponent,
-    TaskDropdownComponent,
+    TaskOverviewComponent,
     TaskPageComponent,
     GanttPageComponent,
     NotFoundPageComponent,
@@ -147,6 +148,7 @@ import { TaskTypePickerComponent } from './components/pickers/task-type-picker/t
     ProgressSpinnerModule,
     TooltipModule,
     MultiSelectModule,
+    DragDropModule,
     RouterModule.forRoot([
       { path: '', component: HomePageComponent, pathMatch: 'full' },  // landing page for site
       { path: 'login', component: LoginComponent},
