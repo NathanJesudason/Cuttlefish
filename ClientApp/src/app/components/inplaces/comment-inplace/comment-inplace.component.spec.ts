61273cc0891944e320bd567d3d861b22265de91b
Sebastian Hardin <sebastian.hardin@gmail.com>
2023-05-13 00:25:12 -0700

    Update unit tests

--- ClientApp/src/app/components/inplaces/comment-inplace/comment-inplace.component.spec.ts
+++ ClientApp/src/app/components/inplaces/comment-inplace/comment-inplace.component.spec.ts
@@ -7,13 +7,47 @@ import {
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { EditorModule } from 'primeng/editor';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { of } from 'rxjs';
 
 import { CommentInplaceComponent } from './comment-inplace.component';
+
 import { CommentData } from 'src/types/comment';
+import { TeamMember } from 'src/types/team-member.model';
+
+import { CommentService } from 'src/app/services/comment/comment.service';
+import { TeamMemberService } from 'src/app/services/team-member/team-member.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 describe('CommentInplaceComponent', () => {
+  const mockTeamMember: TeamMember = {
+    id: 1,
+    username: 'test',
+    password: 'test',
+    email: '',
+    roles: 'user',
+  };
 
-  beforeEach(async () => MockBuilder(CommentInplaceComponent, [ButtonModule, AvatarModule, EditorModule]));
+  beforeEach(async () => MockBuilder(CommentInplaceComponent, [
+    ButtonModule,
+    AvatarModule,
+    EditorModule,
+    ConfirmDialogModule
+  ])
+    .mock(CommentService, {
+      deleteComment: () => of(),
+    } as Partial<CommentService>)
+    .mock(TeamMemberService, {
+      getTeamMemberById: (id: number) => of(mockTeamMember),
+    } as Partial<TeamMemberService>)
+    .mock(UserService, {
+      getUserName: () => of(mockTeamMember.username),
+    } as Partial<UserService>)
+    .mock(AuthService, {
+      getUsernameFromToken: () => mockTeamMember.username,
+    } as Partial<AuthService>)
+  );
 
   it('should create', () => {
     const mockComment: CommentData = {
--- ClientApp/src/app/components/miscellaneous/comments-section/comments-section.component.spec.ts
+++ ClientApp/src/app/components/miscellaneous/comments-section/comments-section.component.spec.ts
@@ -5,14 +5,51 @@ import {
 } from 'ng-mocks';
 
 import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { of } from 'rxjs';
 
 import { CommentInplaceComponent } from 'src/app/components/inplaces/comment-inplace/comment-inplace.component';
 import { CommentsSectionComponent } from './comments-section.component';
+
 import { CommentData } from 'src/types/comment';
 
+import { CommentService } from 'src/app/services/comment/comment.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { TeamMember } from 'src/types/team-member.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { TeamMemberService } from 'src/app/services/team-member/team-member.service';
+
 describe('CommentsSectionComponent', () => {
-  beforeEach(async () => MockBuilder(CommentsSectionComponent, [ButtonModule])
+  const mockComment: CommentData = {
+    id: 1,
+    content: '<p>Comment 1</p>',
+    commenterId: 1,
+    taskId: 5,
+    lastModified: new Date(),
+  };
+
+  const mockTeamMember: TeamMember = {
+    id: 1,
+    username: 'test',
+    password: 'test',
+    email: '',
+    roles: 'user',
+  };
+  
+  beforeEach(async () => MockBuilder(CommentsSectionComponent, [ButtonModule, DividerModule])
     .mock(CommentInplaceComponent, { export: true })
+    .mock(CommentService, {
+      createComment: () => of(mockComment),
+    } as Partial<CommentService>)
+    .mock(UserService, {
+      getUserName: () => of(mockTeamMember.username),
+    } as Partial<UserService>)
+    .mock(AuthService, {
+      getUsernameFromToken: () => mockTeamMember.username,
+    } as Partial<AuthService>)
+    .mock(TeamMemberService, {
+      getTeamMemberById: (id: number) => of(mockTeamMember),
+    } as Partial<TeamMemberService>)
   );
 
   it('should create', () => {
