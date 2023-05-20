3c44baecbb89bcd2c8fb186a01561877cf8dea37
Sebastian Hardin <sebastian.hardin@gmail.com>
2023-05-19 18:06:21 -0700

    Add comments to types

--- ClientApp/src/types/label.ts
+++ ClientApp/src/types/label.ts
@@ -1,8 +1,22 @@
+/**
+ * All the types and functions related to labels
+ */
+
+
+/**
+ * The data for a label
+ * @property `name` - The name of the label
+ * @property `color` - The color of the label, as a hex string
+ */
 export type LabelData = {
   name: string;
   color: string;
 }
 
+/**
+ * An error to throw when the requested label is not found
+ * @property `labelName` - The name of the label that was not found
+ */
 export class LabelNotFoundError extends Error {
   labelName: string;
   
--- ClientApp/src/types/reset-password.model.ts
+++ ClientApp/src/types/reset-password.model.ts
@@ -1,3 +1,11 @@
+/**
+ * Types and functions related to resetting passwords
+ */
+
+
+/**
+ * Model used for resetting password
+ */
 export class ResetPassword{
     public email!            : string;
     public emailToken!       : string;
